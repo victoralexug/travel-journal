5bd6e4115a00ad9438204a3420388627ad55f7c6
Victor Eze <74573591+victoralexug@users.noreply.github.com>
2023-06-21 13:48:16 +0100

    Update data.js
    updated location icon

--- src/data.js
+++ src/data.js
@@ -7,7 +7,8 @@ export default [
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+        locationIcon: 'https://e7.pngegg.com/pngimages/734/288/png-clipart-johnston-red-ginger-location-port-of-south-louisiana-computer-icons-location-people-heart-thumbnail.png'
     },
     {
         id: "2",
@@ -17,7 +18,8 @@ export default [
         startDate: "27 May, 2021",
         endDate: "8 Jun, 2021",
         description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
-        imageUrl: "https://afar.brightspotcdn.com/dims4/default/e6ac7ee/2147483647/strip/true/crop/972x667+14+0/resize/660x453!/format/webp/quality/90/?url=https%3A%2F%2Fafar-media-production-web.s3.amazonaws.com%2Fbrightspot%2F8e%2Fce%2F07941dddb66db98a32e300446848%2Foriginal-sydney-opera-house.jpg"
+        imageUrl: "https://afar.brightspotcdn.com/dims4/default/e6ac7ee/2147483647/strip/true/crop/972x667+14+0/resize/660x453!/format/webp/quality/90/?url=https%3A%2F%2Fafar-media-production-web.s3.amazonaws.com%2Fbrightspot%2F8e%2Fce%2F07941dddb66db98a32e300446848%2Foriginal-sydney-opera-house.jpg",
+        locationIcon: 'https://e7.pngegg.com/pngimages/734/288/png-clipart-johnston-red-ginger-location-port-of-south-louisiana-computer-icons-location-people-heart-thumbnail.png'
     },
     {
         id: "3",
@@ -27,6 +29,7 @@ export default [
         startDate: "01 Oct, 2021",
         endDate: "18 Nov, 2021",
         description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
-        imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+        imageUrl: "https://source.unsplash.com/3PeSjpLVtLg",
+        locationIcon: 'https://e7.pngegg.com/pngimages/734/288/png-clipart-johnston-red-ginger-location-port-of-south-louisiana-computer-icons-location-people-heart-thumbnail.png'
     }
 ]
